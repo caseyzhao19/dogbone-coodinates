ac125e6d71e0447ce5dd088f7bc537e1aea0cf9e
Casey Zhao <czhao4@uw.edu>
2022-04-17 23:53:08 -0700

    changed the edges to faces

--- dogbone.py
+++ dogbone.py
@@ -3,30 +3,39 @@ from prism import Prism
 from point import Point
 from antiprism import Antiprism
 
-prism = Prism(0.2)
+prism = Prism(2)
 alledges = set(prism.edges)
+alltrianglefaces = [[prism.pointsnamed[0], prism.pointsnamed[1], prism.pointsnamed[2]],
+                    [prism.pointsnamed[3], prism.pointsnamed[4], prism.pointsnamed[5]]]
 #for p, q in alledges: print(p, q)
-antiprism = Antiprism(0.2)
+antiprism = Antiprism(2)
 squarefaces = [prism.face1, prism.face2, prism.face3]
 newsquarefaces = []
 for face in squarefaces:
     antiprism.moveface(2, 0, 4, prism.pointsnamed[face[0]], prism.pointsnamed[face[1]], prism.pointsnamed[face[2]])
     newsquarefaces.append([antiprism.pointsnamed[3], antiprism.pointsnamed[1], antiprism.pointsnamed[5]])
     alledges = alledges.union(antiprism.edges)
-for i in range(5):
+    for i in range(8):
+        alltrianglefaces.append([antiprism.pointsnamed[i], antiprism.pointsnamed[(i+1) % 8],
+                                 antiprism.pointsnamed[(i+2) % 8]])
+for i in range(0):
     squarefaces = []
     for face in newsquarefaces:
         prism.moveface(0, 3, 2, face[0], face[1], face[2])
         squarefaces.append([prism.pointsnamed[2], prism.pointsnamed[1], prism.pointsnamed[5]])
         squarefaces.append([prism.pointsnamed[1], prism.pointsnamed[0], prism.pointsnamed[4]])
         alledges = alledges.union(prism.edges)
+        alltrianglefaces.append([prism.pointsnamed[0], prism.pointsnamed[1], prism.pointsnamed[2]])
+        alltrianglefaces.append([prism.pointsnamed[3], prism.pointsnamed[4], prism.pointsnamed[5]])
 
     newsquarefaces = []
     for face in squarefaces:
         antiprism.moveface(2, 0, 4, face[0], face[1], face[2])
         newsquarefaces.append([antiprism.pointsnamed[3], antiprism.pointsnamed[1], antiprism.pointsnamed[5]])
         alledges = alledges.union(antiprism.edges)
-
+        for j in range(8):
+            alltrianglefaces.append([antiprism.pointsnamed[j], antiprism.pointsnamed[(j+1) % 8],
+                                     antiprism.pointsnamed[(j+2) % 8]])
 
 '''antiprism.moveface(2, 0, 4, prism.pointsnamed[3], prism.pointsnamed[0], prism.pointsnamed[5])
 alledges = alledges.union(antiprism.edges)
@@ -47,7 +56,24 @@ alledges = alledges.union(prism.edges)'''
 
 
 print(len(alledges))
-for edge in alledges:
+print(len(alltrianglefaces))
+
+for a, b, c in alltrianglefaces:
+    d = a.inverse()
+    center, radius = fourpointsphere(a,b,c,d)
+    print("intersection {")
+    print("\t sphere { <", center.x, ",", center.y, ",", center.z, ">,", radius)
+    print("\t\t texture{ pigment{ color rgb<1,0.7,0.1> }")
+    print("\t\t\t finish { specular .7 }}} ")
+    print("\t clipped_by {mesh {")
+    O = Point(0, 0, 0)
+    for p,q,r in [(a,b,c), (O, a, b) , (O, a, c), (O, b, c)]:
+        print("\t\t triangle { <", p.x, ",", p.y, ",", p.z, ">, <", q.x, ",", q.y, ",", q.z, ">, <",
+              r.x, ",", r.y, ",", r.z, "> }")
+    print("\t\t inside_vector <0, 0, 1>}}")
+    print("}")
+
+'''for edge in alledges:
     c1, c2 = edge
     c3 = c2.inverse()
     if c3 is None:
@@ -63,4 +89,4 @@ for edge in alledges:
         c3 = sdiff.plus(c)
     print(str(c1.x) + ", " + str(c1.y) + ", " + str(c1.z) + ", " +
           str(c2.x) + ", " + str(c2.y) + ", " + str(c2.z) + ", " +
-          str(c3.x) + ", " + str(c3.y) + ", " + str(c3.z) + ", ")
+          str(c3.x) + ", " + str(c3.y) + ", " + str(c3.z) + ", ")'''
