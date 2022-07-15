d3c0ecf584f2492971fd918370f93e457448eb7d
Casey Zhao <czhao4@uw.edu>
2022-07-15 01:11:43 -0700

    did more stuff

--- addsomezeroestofilename.py
+++ addsomezeroestofilename.py
@@ -1,10 +1,5 @@
 import os
 
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# DO NOT RUN UNTIL YOU HAVE MADE A COPY OF PLOTS SO THAT IF THE RENAMING IS BOTCHED YOU DON'T LOSE ALL YOUR WORK
-# ALSO, REMEMBER TO COPY THE PRINTOUT FROM DOG BONE TO YOUR TEXT DOCUMENT
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
 for file in os.listdir("./plots"):
     num = file[4:-4]
     if 0 <= int(num) <= 9:
